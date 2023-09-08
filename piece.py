f1fdc01c2bfe0e6a5d4f559ab239552ad1929216
juggernaut2005ua <104765323+juggernaut2005ua@users.noreply.github.com>
2023-09-08 21:57:26 +0200

    kaif

--- piece.py
+++ piece.py
@@ -13,11 +13,11 @@ class Piece:
     def is_piece_picked(self):
         return self.isPicked
 
-    def is_valid_move(self, new_col, new_row, board):
-        if self.move_possibility(new_col, new_row):
-            piece_at_new_position = board.get_piece_at(new_row, new_col)
-            return piece_at_new_position is None or piece_at_new_position.get_color() != self.get_color()
-        return False
+    # def is_valid_move(self, new_col, new_row, board):
+    #     if self.move_possibility(new_col, new_row):
+    #         piece_at_new_position = board.get_piece_at(new_row, new_col)
+    #         return piece_at_new_position is None or piece_at_new_position.get_color() != self.get_color()
+    #     return False
 
     def move_possibility(self, col, row):
         return 0 <= col < 8 and 0 <= row < 8
@@ -30,6 +30,24 @@ class Piece:
 
     def get_type(self):
         return self.__class__.__name__
+    
+    def move(self, board_logic, new_col, new_row):
+        # Ваша логика проверки допустимости хода
+        if self.is_valid_move(board_logic, new_col, new_row):
+            # Переместить фигуру на новую позицию
+            self.set_position(new_col, new_row)
+            return True
+        else:
+            return False
+        
+    def move_piece(self, piece_to_move, new_col, new_row):
+        # Проверяем, допустим ли такой ход
+        if piece_to_move.is_valid_move(self, new_col, new_row):
+            # Выполняем перемещение фигуры
+            piece_to_move.move(self, new_col, new_row)
+            return True
+        else:
+            return False
 
 
 class Pawn(Piece):
@@ -84,14 +102,26 @@ class Pawn(Piece):
         # Якщо жодна з умов вище не виконується, хід вважається допустимим.
         return True
 
-    def move_piece(self, piece_to_move, new_col, new_row):
-        # Проверяем, допустим ли такой ход
-        if piece_to_move.is_valid_move(self, new_col, new_row):
-            # Выполняем перемещение фигуры
-            piece_to_move.move(self, new_col, new_row)
-            return True
-        else:
-            return False
+
+    # def move(self, board_logic, new_col, new_row):
+    #     # Ваша логика проверки допустимости хода
+    #     if self.is_valid_move(board_logic, new_col, new_row):
+    #         # Переместить фигуру на новую позицию
+    #         self.set_position(new_col, new_row)
+    #         return True
+    #     else:
+    #         return False
+        
+    # def move_piece(self, piece_to_move, new_col, new_row):
+    #     # Проверяем, допустим ли такой ход
+    #     if piece_to_move.is_valid_move(self, new_col, new_row):
+    #         # Выполняем перемещение фигуры
+    #         piece_to_move.move(self, new_col, new_row)
+    #         return True
+    #     else:
+    #         return False
+        
+    
 
 
 
@@ -114,6 +144,29 @@ class Knight(Piece):
             return "images/whiteKnight.png"
         else:
             return "images/blackKnight.png"
+        
+    def is_valid_move(self, board_logic, new_col, new_row):
+        current_col, current_row = self.get_piece_coordinates()
+        piece_color = self.get_color()
+
+        for piece, (col, row) in board_logic.pieces.items():
+            # Перевірте, чи цільовий квадрат зайнятий іншою фігурою того ж кольору.
+            if col == new_col and row == new_row and piece.get_color() == piece_color:
+                return False
+
+        moves = [
+            (current_col + 1, current_row - 2), (current_col + 2, current_row - 1),
+            (current_col + 2, current_row + 1), (current_col + 1, current_row + 2),
+            (current_col - 1, current_row + 2), (current_col - 2, current_row + 1),
+            (current_col - 2, current_row - 1), (current_col - 1, current_row - 2)
+        ]
+
+        # Перевірка, чи нова позиція знаходиться серед можливих ходів коня
+        if (new_col, new_row) in moves:
+            return True
+
+        return False
+
 
 
 class Bishop(Piece):
@@ -158,6 +211,24 @@ class Bishop(Piece):
             return "images/whiteBishop.png"
         else:
             return "images/blackBishop.png"
+        
+    def is_valid_move(self, piece_to_move, new_col, new_row):
+        current_col, current_row = piece_to_move.get_piece_coordinates()
+        piece_color = piece_to_move.get_color()
+
+        if abs(current_col - new_col) == abs(current_row - new_row):
+            col_step = 1 if new_col > current_col else -1
+            row_step = 1 if new_row > current_row else -1
+            col, row = current_col + col_step, current_row + row_step
+            while col != new_col:
+                if self.get_piece_at(row, col):
+                    return False
+                col += col_step
+                row += row_step
+            return True  # Повернути True, якщо це можливий хід для слона
+        else:
+            return False
+
 
 
 class Rook(Piece):
@@ -181,6 +252,29 @@ class Rook(Piece):
             return "images/whiteRook.png"
         else:
             return "images/blackRook.png"
+        
+    def is_valid_move(self, piece_to_move, new_col, new_row):
+        current_col, current_row = piece_to_move.get_piece_coordinates()
+        piece_color = piece_to_move.get_color()
+
+        for piece, (col, row) in self.pieces.items():
+            # Перевірте, чи цільовий квадрат зайнятий іншою фігурою того ж кольору.
+            if col == new_col and row == new_row and piece.get_color() == piece_color:
+                return False
+            if current_col == new_col or current_row == new_row:
+                # Проверка наличия фигур на пути
+                if current_col == new_col:
+                    min_row, max_row = min(current_row, new_row), max(current_row, new_row)
+                    for row in range(min_row + 1, max_row):
+                        if self.get_piece_at(row, current_col):
+                            return False
+                else:
+                    min_col, max_col = min(current_col, new_col), max(current_col, new_col)
+                    for col in range(min_col + 1, max_col):
+                        if self.get_piece_at(current_row, col):
+                            return False
+                        
+        return True 
 
 
 class Queen(Piece):
@@ -234,6 +328,41 @@ class Queen(Piece):
             return "images/whiteQueen.png"
         else:
             return "images/blackQueen.png"
+        
+    def is_valid_move(self, piece_to_move, new_col, new_row):
+        current_col, current_row = piece_to_move.get_piece_coordinates()
+        piece_color = piece_to_move.get_color()
+
+        for piece, (col, row) in self.pieces.items():
+            # Перевірте, чи цільовий квадрат зайнятий іншою фігурою того ж кольору.
+            if col == new_col and row == new_row and piece.get_color() == piece_color:
+                return False
+            # Проверка вертикальных и горизонтальных ходов, аналогично ладье
+            if current_col == new_col or current_row == new_row:
+                # Проверка наличия фигур на пути
+                if current_col == new_col:
+                    min_row, max_row = min(current_row, new_row), max(current_row, new_row)
+                    for row in range(min_row + 1, max_row):
+                        if self.get_piece_at(row, current_col):
+                            return False
+                else:
+                    min_col, max_col = min(current_col, new_col), max(current_col, new_col)
+                    for col in range(min_col + 1, max_col):
+                        if self.get_piece_at(current_row, col):
+                            return False
+            # Проверка диагональных ходов, аналогично слону
+            elif abs(current_col - new_col) == abs(current_row - new_row):
+                col_step = 1 if new_col > current_col else -1
+                row_step = 1 if new_row > current_row else -1
+                col, row = current_col + col_step, current_row + row_step
+                while col != new_col:
+                    if self.get_piece_at(row, col):
+                        return False
+                    col += col_step
+                    row += row_step
+            else:
+                return False
+        return True
 
 
 class King(Piece):
@@ -255,4 +384,24 @@ class King(Piece):
             return "images/whiteKing.png"
         else:
             return "images/blackKing.png"
+        
+    def is_valid_move(self, piece_to_move, new_col, new_row):
+        current_col, current_row = piece_to_move.get_piece_coordinates()
+        piece_color = piece_to_move.get_color()
+
+        for piece, (col, row) in self.pieces.items():
+            # Перевірте, чи цільовий квадрат зайнятий іншою фігурою того ж кольору.
+            if col == new_col and row == new_row and piece.get_color() == piece_color:
+                return False
+            moves = [
+                (current_col, current_row - 1), (current_col + 1, current_row - 1),
+                (current_col + 1, current_row), (current_col + 1, current_row + 1),
+                (current_col, current_row + 1), (current_col - 1, current_row + 1),
+                (current_col - 1, current_row), (current_col - 1, current_row - 1)
+            ]
+            if (new_col, new_row) in moves:
+                return True
+            return False
+        
+        return True
 
